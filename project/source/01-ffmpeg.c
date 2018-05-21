f26c2e419d3f7c1c4bb895c287e9bbce46b50248
caohang <caohang@henggucn.com>
2018-05-21 10:19:19 +0800

    add ffmpeg01

--- project/source/01-ffmpeg.c
+++ project/source/01-ffmpeg.c
@@ -36,9 +36,183 @@ int parse_options(struct options*opts,int argc,char**argv){
 	opts->thread_count=0;
 	strcpy(opts->finput,argv[1]);
 	optidx=2;
-	while(){
+	while(optidx<argc){
+        optstr=argv[optidx++];
+        if(*optstr++!='-')return -1;
+        switch(*optstr++){
+            case 's':  //stream id
+                opts->streamId=atoi(optstr);
+                break;
+            case 'f'://frames
+                opts->frames=atoi(optstr);
+                break;
+            case 'k': //skipped
+                opts->lstart=atoll(oopstr);
+                break;
+            case 'o'://ouput
+                strcpy(opts->foutput1,optstr);
+                strcat(opts->fouput1,".mpg");
+                strcpy(opts->foutput2,optstr);
+                strcat(opts->foutput2,".raw");
+            case 'n'://decoding and output options
+                if(strcmp("dec",optstr)==0){
+                    opts->nodec=1;
+                }
+                break;
+            case 'p':
+                opts->bplay=1;
+                break;
+            case 't':
+                opts->thread_count=atoi(optstr);
+                break;
+            default:
+                return -1;
+        }
 	}
+    return 0;
 }
+
+void show_help(char* program){
+    printf("简单的FFMPEG测试方案\n");
+    printf("Usage:%s inputfile [-sstreamid] [-fframes] [-kskipped] 
+            [-ooutput_filename(without extension)] [-p] [-tthread_count]\n");
+    return ;
+}
+static void log_callback(void*ptr,int level,const char*fmt, va_list vl){
+    vfprintf(stdout,fmt,vl);
+}
+
+/* #################音频渲染器的代码(OSS)########################## */
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/soundcard.h>
+#define OSS_DEVICE "/dv/dsp0"
+
+struct audio_dsp{
+    int audio_fd;
+    int channels;
+    int format;
+    int speed;
+};
+int map_formats(enum SampleFromat format){
+    switch(format){
+        case SAMPLE_FMT_U8:
+            return AFMT_U8;
+        case SAMPLE_FMT_S16:
+            return AFMT_S16_LE;
+        default:
+            return AFMT_U8;
+    }
+}
+
+int set_audio(struct audio_dsp*dsp){
+    if(dsp->audio_fd==-1){
+        printf("无效的音频DSP ID！");
+        return -1;
+    }
+    if(-1==ioctl(dsp->audio_fd,SNDCTL_DSP_SETFMT,&dsp->format)){
+        printf("无法设置DSP格式\n");
+        return -1;
+    }
+    if(-1==ioctl(dsp->audio_fd,SNDCTL_DSP_SPEED,&dsp->speed)){
+        printf("无法设置DSP格式\n");
+        return -1;
+    }
+    return 0;
+}
+int play_pcm(struct audio_dsp*dsp,unsigned char*buf,int size){
+    if(dsp->audio_fd==-1){
+        printf("无效的音频DSP ID \n");
+        return -1;
+    }
+    if(-1==write(dsp->audio_fd,buf,size)){
+        printf("音频DSP 无法写入 \n");
+        return -1;
+    }
+    return 0;
+}
+
+/*##########################视频渲染的代码##############################*/
+#include <linux/fb.h>
+#include <sys/mman.h>
+
+#define FB_DEVICE "/dev/fb0"
+enum pic_format{
+    eYUV_420_Planer,
+};
+struct video_fb{
+    int video_fd;
+    struct fb_var_screeninfo vinfo;
+    struct fb_fix_screeninfo finfo;
+    unsigned char*fbp;
+    AVFrame* frameRGB;
+    struct{
+        int x;
+        int y;
+    }video_pos;
+};
+int open_video(struct video_fb*fb,int x,int y){
+    int screensize;
+    fb->video_fd=open(FB_DEVICE,O_WRONLY);
+    if(fb->video_fd==-1)return -1;
+    if(ioctl(fb->video_fd,FBIOGET_FSCREENINFO,&fb->finfo))return -2;
+    if(ioctl(fb->video_fd,FBIOGET_VSCREENINFO,&fb->vinfo))return -2;
+    printf("视频设备:分解 %dx%d,%dbpp\n",fb->vinfo.xres,fb->vinfo.yres,fb->vinfo.bits_per_pixel);
+    //获得字节数,bpp(一个像素多少字节)
+    screensize=fb->vinfo.xres*fb->vinfo.yres*fb->vinfo.bits_per_pixel/8;
+    fb->fbp=(unsigned char*)mmap(0,screensize,PROT_READ|PROT_WRITE,MAP_SHARED,fb->video_fb,0);
+    if(fb->fbp==-1)return -3;
+    if(x>=fb->vinfo.xres||y>=fb->vinfo.yres){
+        return -4;
+    }else{
+        fb->video_pos.x=x;
+        fb->video_pos.y=y;
+    }
+    fb->frameRGB=avcodec_alloc_frame();
+    if(!fb->frameRGB)return -5;
+    return 0;
+}
+#if 0
+/**********only 420P supported now***********/
+int show_picture(struct video_fb*fb,AVFrame* frame,int width,int height,enum pic_format formt){
+   struct SwsContext* sws;
+   int i;
+   unsigned char* dest;
+   unsigned char* src; 
+   if(fb->video_fd==-1)return -1;
+   if(fb->video_pos.x>=fb->vinfo.xres||fb->video_pos.y>=fb->vinfo.yres)return -2;
+   if(fb->video_pos.x+width>fb->vinfo.xres){
+       width=fb->vinfo.xres-fb->video_pos.x; 
+   }
+   if(fb->video_pos.y+height>fb->vinfo.yres){
+        height=fb->vinfo.yres-fb->video_pos.y;
+   }
+   if(format==PIX_FMT_YUV420P){
+        sws=sws_getContext(width,height,format,width,height,PIX_FMT_RGB32,SWS_FAST_RGB32,SWS_FAST_BILINEAR,NULL,NULL,NULL);
+   }
+   if(sws==0){
+        return -3;
+   }
+   if(sws_scale(sws,frame->data,frame->linesize,0,height,fb->frameRGB->data,fb->frameRGB->linesize)){
+        return -3;
+   }
+   dest=fb->fbp+(fb->video_pox.x+fb->vinfo.xoffset)*(fb->vinfo.bits_per_pixel/8)+(fb->video_pos.y+fb->vinfo.yoffset)*fb->finfo.line_length;;
+   for(i=0;i<height;i++){
+        memcpy(dest,src,width*4);
+        src+=fb->frameRGB->linesize[0];
+        dest+=fb->finfo.line_length;
+   }
+   return 0;
+}
+void close_video(struct video_fb*fb){
+    if(fb->video_fd!=-1){
+        munmap(fb->fbp,fb->vinfo.xres*fb->vinfo.yres*fb->vinfo.bits_per_pixel/8);
+        close(fb->video_fd);
+        fb->video_fd=-1;
+    }
+}
+#endif
 
 int main(int argc,char**argv){
 	AVFormatContext* pCtx=0;
