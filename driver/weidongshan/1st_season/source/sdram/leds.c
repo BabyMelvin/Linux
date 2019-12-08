8820cab8606a12d80690ed514e5e06fe73a35283
hang <862214485@qq.com>
2019-12-08 20:30:55 +0800

    add sdram

---
+++ driver/weidongshan/1st_season/source/sdram/leds.c
@@ -0,0 +1,27 @@
+#define GPFCON    (*(volatile unsigned long *)0x56000050)
+#define GPFDAT    (*(volatile unsigned long *)0x56000054)
+
+#define GPF4_out   (1<<(4*2))
+#define GP5_out    (1<<(5*2))
+#define GP6_out    (1<<(6*2))
+
+void wait(volatile unsigned long dly)
+{
+    for(;dly > 0; dly--);
+}
+
+int main(void)
+{
+    unsigned long i = 0;
+
+    GPFCON = GPF4_out | GP5_out | GP6_out;
+    while (1) {
+        wait (30000);
+        GPFDAT = (~(i << 4));
+        if (++i == 8)
+            i = 0;
+
+    }
+
+    return 0;
+}
