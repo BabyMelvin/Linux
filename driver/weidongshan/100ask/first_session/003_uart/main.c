04a8bd2a9b7e1607ed1c97afb9ee33ad0a7ea2b5
caohang <melvin_cao@sileadinc.com>
2020-08-22 21:35:04 +0800

    增加uart 串口程序

---
+++ driver/weidongshan/100ask/first_session/003_uart/main.c
@@ -0,0 +1,32 @@
+#include "s3c2440_soc.h"
+#include "uart.h"
+
+int main (void)
+{
+    unsigned char c;
+
+    uart0_init();
+
+    putchar('A');
+    puts("Hello, world!\n\r");
+
+    /* 回显，将收到字符原本的在显示出来 
+     * 比如输入A -> 则 A发送给串口，此时不会显示
+     *              main通过getchar然后putchar回显给PC
+     * */
+    while (1) {
+        c = getchar();
+
+        if (c == '\r') {
+            putchar('\n');
+        }
+
+        if (c == '\n') {
+            putchar('\r');
+        }
+
+        putchar(c);
+    }
+
+    return 0;
+}
