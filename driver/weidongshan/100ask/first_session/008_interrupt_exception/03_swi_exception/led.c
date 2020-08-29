82690190826099544d83898ad78e59778b170201
caohang <melvin_cao@sileadinc.com>
2020-08-29 21:52:02 +0800

    增加swi 异常

---
+++ driver/weidongshan/100ask/first_session/008_interrupt_exception/03_swi_exception/led.c
@@ -0,0 +1,50 @@
+void delay(volatile int d)
+{
+    //int i = 0;
+    //for (i = 0; i < 10; i++)
+        while (d--);
+}
+
+#ifdef LED_PARAMS
+int led_on(int which)
+{
+    unsigned int *p_GPFCON = (unsigned int *)0x56000050;
+    unsigned int *p_GPFDAT = (unsigned int *)0x56000054;
+
+    if (which == 4) {
+        /* 配置GPF4为输出引脚*/
+        *p_GPFCON = 0x100;
+    } else {
+        /* 配置GPF5为输出引脚*/
+        *p_GPFCON = 0x400;
+    }
+
+    /* 设置GPF4/GPF5输出0 */
+    *p_GPFDAT = 0;
+
+    return 0;
+}
+#endif
+
+int led_test()
+{
+    volatile unsigned int *p_GPFCON = (volatile unsigned int *)0x56000050;
+    volatile unsigned int *p_GPFDAT = (volatile unsigned int *)0x56000054;
+
+    int val = 0, tmp;
+
+    //将4/5/6配置为输出引脚
+    *p_GPFCON &= ~((3 << 8) | (3 << 10) | (3 << 12));
+    *p_GPFCON |= ((1 << 8) | (1 << 10) | (1 << 12));
+
+    while (1) {
+        tmp = ~(1 << val);
+        *p_GPFDAT &= ~(7 << 4);
+        *p_GPFDAT |= (tmp << 4);
+        delay(100000);
+        val ++;
+        if (val == 3) val = 0;
+    }
+
+    return 0;
+}
---
+++ driver/weidongshan/100ask/first_session/008_interrupt_exception/03_swi_exception/uart.h
@@ -0,0 +1,8 @@
+#ifndef __UART_H__
+#define __UART_H__
+
+void uart0_init();
+int putchar (int c);
+int getchar (void);
+int puts(const char *s);
+#endif /* __UART_H__*/
