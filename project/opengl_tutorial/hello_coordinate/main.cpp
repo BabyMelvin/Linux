20378d90661ce0a4feaea0fdd24be61c5ddf46fd
caohang <melvin_cao@sileadinc.com>
2020-09-13 17:48:28 +0800

    10 cutes show

--- project/opengl_tutorial/hello_coordinate/main.cpp
+++ project/opengl_tutorial/hello_coordinate/main.cpp
@@ -183,6 +183,17 @@ int main(int argc, char *argv[])
     //     0, 1, 3, // first Triangle
     //     1, 2, 3  // second Triangle
     // };
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f)};
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -291,7 +302,7 @@ int main(int argc, char *argv[])
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         // also clear the depth buffer now!
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
+
         glActiveTexture(GL_TEXTURE0); // 在绑定纹理之前先激活纹理单元
         glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE1);
@@ -299,7 +310,7 @@ int main(int argc, char *argv[])
         //glm::mat4 trans(1.0f);
         //trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
         //trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-        
+
         // draw our first triangle
         glUseProgram(shaderProgram);
         glm::mat4 model = glm::mat4(1.0f);
@@ -325,7 +336,16 @@ int main(int argc, char *argv[])
         // seeing as we only have a single VAO
         //there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model;
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        //glDrawArrays(GL_TRIANGLES, 0, 36);
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // glBindVertexArray(0); // no need to unbind it every time
